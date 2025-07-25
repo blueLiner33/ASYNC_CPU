6fda5be7d8e12ec1ee74ecfac897ad0874a19617
blueLiner33 <163341562+blueLiner33@users.noreply.github.com>
2025-07-25 00:18:38 -0400

    hiding

---
+++ Main/MVP/own_handshake_mvp.v
@@ -0,0 +1,16 @@
+module four_phase_hand(
+    input in_ack,
+    input in_req,
+    input [15:0] in_data,
+    input done,
+
+    output out_ack,
+    output out_req,
+    output [15:0] out_data
+
+);
+
+
+
+
+endmodule
