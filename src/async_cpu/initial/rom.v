cf83ee169c7d8793397cd49ad6d3b281b8ceb9db
blueLiner33 <163341562+blueLiner33@users.noreply.github.com>
2025-07-31 16:01:20 -0400

    issue 1 fix

--- src/async_cpu/initial/rom.v
+++ src/async_cpu/initial/rom.v
@@ -1,6 +1,6 @@
 module instr_rom #(
     parameter ADDR_WIDTH = 8,
-    parameter DATA_WIDTH = 16
+    parameter DATA_WIDTH = 32
 )(
     input  wire [ADDR_WIDTH-1:0] addr,
     output reg  [DATA_WIDTH-1:0] instr
